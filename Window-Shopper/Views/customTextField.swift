31ae8bfdfe3e2ac82e04f35b69983c8e32becb21
JoeyTribbiani <vophanhongdung@gmail.com>
2018-03-16 10:06:26 +0700

    drawing in textfield

--- Window-Shopper/Views/customTextField.swift
+++ Window-Shopper/Views/customTextField.swift
@@ -12,6 +12,23 @@ import UIKit
 class customTextField: UITextField {
 
     
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        let currencylbl = UILabel(frame : CGRect(x: 5, y: (frame.size.height / 2) - size / 2 , width: size, height: size))
+        currencylbl.backgroundColor = #colorLiteral(red: 0.7913229695, green: 0.7913229695, blue: 0.7913229695, alpha: 0.8)
+        currencylbl.textAlignment = .center
+        currencylbl.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        currencylbl.layer.cornerRadius = 5.0
+        currencylbl.clipsToBounds = true
+
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        
+        currencylbl.text = formatter.currencySymbol
+        addSubview(currencylbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeTextField()
     }
@@ -27,6 +44,8 @@ class customTextField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.1964094606)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        
+        clipsToBounds  = true
         
         if let placeHolder = placeholder {
             let place = NSAttributedString(string: placeHolder, attributes: [.foregroundColor : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
